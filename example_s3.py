bbc5c345c8760a152d67b823fb17204d527dc9dd
GissellPenado <gpenadom@uamv.edu.ni>
2025-04-02 13:22:37 -0600

    Ejemplo1.py

---
+++ example_s3.py
@@ -0,0 +1,6 @@
+#Calcular nota
+Sistematico1=int(input("Ingrese el primer sistematico (max:30): "))
+Sistematico2=int(input("Ingrese el segundo sistematico (max:30): "))
+Sistematico3=int(input("Ingrese el tercer sistematico(max:40): "))
+nota= (Sistematico1 + Sistematico2 + Sistematico3)
+print("La nota del estudiante es: ", nota)
